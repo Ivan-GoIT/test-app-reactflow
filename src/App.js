a307db2c6d34cd12c7a4373d97dc943f52c2cb5e
IvanKoshel <educationZak@gmail.com>
2023-07-27 22:06:26 +0300

    to do - create logic with display by multiple nodes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  NodeToolbar
+} from 'reactflow';
 
-function App() {
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1',type: 'input', position: { x: 150, y: 100 }, data: { label: '1' } ,},
+  { id: '2', position: { x: 50, y: 200 }, data: { label: '2' } },
+  { id: '3', position: { x: 250, y: 200 }, data: { label: '3' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' ,type:'step',arrowHeadType: 'none'},{ id: 'e2-3', source: '1', target: '3',type:'step',arrowHeadType: 'none'}];
+
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <NodeToolbar />
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
     </div>
   );
 }
-
-export default App;
